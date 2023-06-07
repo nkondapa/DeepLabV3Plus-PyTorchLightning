ce35f183ceab813ad1a9352a14d38ffe5d11862b
Neehar Kondapaneni <nkondapa@caltech.edu>
2023-06-07 00:41:09 -0700

    184 epoch experiment -> for 30k iterations

--- deeplabv3_run.sh
+++ deeplabv3_run.sh
@@ -1,2 +1,3 @@
 #python train_deeplabv3plus.py --eval_dataset pascal --max_epochs 50 --batch_size 16 --num_gpus 4 --num_workers 16  --val_every_n_epochs 3 --wandb_group deeplabv3plus_training
-python train_deeplabv3plus.py --eval_dataset pascal --max_epochs 100 --batch_size 16 --num_gpus 4 --num_workers 16  --val_every_n_epochs 3 --wandb_group deeplabv3plus_training
+#python train_deeplabv3plus.py --eval_dataset pascal --max_epochs 100 --batch_size 16 --num_gpus 4 --num_workers 16  --val_every_n_epochs 3 --wandb_group deeplabv3plus_training
+python train_deeplabv3plus.py --eval_dataset pascal --max_epochs 184 --batch_size 16 --num_gpus 4 --num_workers 16  --val_every_n_epochs 3 --wandb_group deeplabv3plus_training
