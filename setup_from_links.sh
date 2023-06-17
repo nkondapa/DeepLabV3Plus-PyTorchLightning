942043e008757c19d2e5e0242f42455ffb618e2e
Rogério Júnior <rogerio.agjunior@gmail.com>
2023-06-17 16:20:03 -0700

    Update setup_from_links.sh
    Downloading only cityscapes

--- setup_from_links.sh
+++ setup_from_links.sh
@@ -1,24 +1,24 @@
-#sudo apt-get update && apt-get install ffmpeg libsm6 libxext6 unar vim htop -y gcc zip unzip
+sudo apt-get update && apt-get install ffmpeg libsm6 libxext6 unar vim htop -y gcc zip unzip
+
+#git clone https://github.com/nkondapa/DeeplabV3PlusPL.git
+#cd DeeplabV3PlusPL
+mkdir data
+
+pip install virtualenv
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+#wget -P data/ http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
+#tar -xvf data/VOCtrainval_11-May-2012.tar -C ./data/
 #
-##git clone https://github.com/nkondapa/DeeplabV3PlusPL.git
-##cd DeeplabV3PlusPL
-#mkdir data
+#rm -rf data/VOCdevkit/VOC2012/SegmentationClass
+#wget -O data/SegmentationClassAug.zip https://www.dropbox.com/s/oeu149j8qtbs1x0/SegmentationClassAug.zip?dl=0
+#unzip data/SegmentationClassAug.zip "SegmentationClassAug/*" -d ./data
+#mv data/SegmentationClassAug data/VOCdevkit/VOC2012/SegmentationClass
 #
-#pip install virtualenv
-#virtualenv venv
-#source venv/bin/activate
-#pip install -r requirements.txt
+#mv trainaug.txt data/VOCdevkit/VOC2012/ImageSets/Segmentation/
 
-wget -P data/ http://host.robots.ox.ac.uk/pascal/VOC/voc2012/VOCtrainval_11-May-2012.tar
-tar -xvf data/VOCtrainval_11-May-2012.tar -C ./data/
-
-rm -rf data/VOCdevkit/VOC2012/SegmentationClass
-wget -O data/SegmentationClassAug.zip https://www.dropbox.com/s/oeu149j8qtbs1x0/SegmentationClassAug.zip?dl=0
-unzip data/SegmentationClassAug.zip "SegmentationClassAug/*" -d ./data
-mv data/SegmentationClassAug data/VOCdevkit/VOC2012/SegmentationClass
-
-mv trainaug.txt data/VOCdevkit/VOC2012/ImageSets/Segmentation/
-
-#pip install gdown
-#gdown 1x8V3cPQdDKAXP2gxUG3g2APcUXzPG4aM -O data/cityscapes.tar.gz
-#tar -xvf data/cityscapes.tar.gz -C data
+pip install gdown
+gdown 1x8V3cPQdDKAXP2gxUG3g2APcUXzPG4aM -O data/cityscapes.tar.gz
+tar -xvf data/cityscapes.tar.gz -C data
