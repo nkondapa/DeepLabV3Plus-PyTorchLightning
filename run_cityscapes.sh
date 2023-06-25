007853e0b1d3961e1e810534e13e239d7d9a84ec
root <rogerio@caltech.edu>
2023-06-25 02:00:28 +0000

    changing to 80k steps

--- run_cityscapes.sh
+++ run_cityscapes.sh
@@ -1,13 +1,13 @@
 python train_deeplabv3plus_new.py \
 --train_dataset cityscapes \
 --val_dataset cityscapes \
---max_steps 40000 \
+--max_steps 80000 \
 --batch_size 8 \
 --num_gpus 4 \
 --num_workers 16  \
 --val_every_n_epochs 3 \
 --wandb_group deeplabv3plus_cityscapes \
---exp_name deeplabv3plus_new_cityscapes_bs8_steps40k_lr1e-2 \
+--exp_name deeplabv3plus_new_cityscapes_bs8_steps80k_lr1e-2 \
 --backbone resnet101 \
 --lr 0.01  \
 --crop_size 768 \
